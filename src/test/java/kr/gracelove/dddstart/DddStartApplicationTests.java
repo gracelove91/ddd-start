ea1f6723d24eaa057c8f1314115328b657152c66
gracelove <govlmo91@gmail.com>
2020-03-11 10:07:30 +0900

    init

---
+++ src/test/java/kr/gracelove/dddstart/DddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gracelove.dddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
